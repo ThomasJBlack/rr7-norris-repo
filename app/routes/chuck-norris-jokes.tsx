945977b5c32cf5e0381ab1882b15ea642b34e0e3
Thomas Black <tomjblack250@gmail.com>
2025-03-14 15:42:21 -0700

    added variable type examples: string, number, object (object & array... stuff)

--- app/routes/chuck-norris-jokes.tsx
+++ app/routes/chuck-norris-jokes.tsx
@@ -17,15 +17,51 @@ export async function loader() {
         },
     })).json() as NorrisResponse;
 
-    return ({ response, url: chuckNorrisJokeURL });
+    const objVar = {
+        key: "value",
+        key2: {
+            anohterkey: "value2"
+        },
+        key3: {
+            anohterkey: "value2"
+        },
+        key4: {
+            anohterkey: "value2"
+        },
+        key5: {
+            anohterkey: "value2"
+        },
+        anArray: [
+            { key0: "value" },
+            { key1: 100 },
+            { key2: { anotherObj: "value3" } },
+            { key3: ["value4"] }
+        ]
+    }
+
+    return ({ response, url: chuckNorrisJokeURL, objVar });
 }
 
 export default function Test({ loaderData }: Route.ComponentProps) {
-    // console.log(`asdf 🧐 ~ loaderData:`, loaderData)
+    const arrayVal = loaderData.objVar.anArray
+
     return (
         <div className="p-20">
             <h1 className="py-2">Chuck Norris Jokes</h1>
             <code>{loaderData.response.value}</code>
+
+            <h2>types</h2>
+            <br />
+            {arrayVal.map((item) => {
+                return <>
+                    <div>{JSON.stringify(item)}</div>
+                    <div>{JSON.stringify(typeof item)}</div>
+                    Item Value:
+                    <div>{JSON.stringify(Object.values(item)[0])}</div>
+                    <div>{JSON.stringify(typeof Object.values(item)[0])}</div>
+                    <br/>
+                </>
+            })}
         </div>
     )
 }
